76112a613152603eba7f8461beef396712ee7955
Chol27 <TentChonlakorn@gmail.com>
2022-03-18 16:01:39 +0700

    clean up

---
+++ database/seed-data/init.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8476ff4b21a8535438e666831d24430b24e53e0f43056b09fa11df0b2fd60cbd
+size 9019
--- server/src.sql
+++
@@ -1,3 +0,0 @@
-CLUSTER message USING create_action_id_idx;
-
-CLUSTER action USING action_type_idx;
